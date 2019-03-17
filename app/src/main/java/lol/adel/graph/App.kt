d41ccac2c16d4369d9820c3c63e9e7aeee064707
adelnizamutdinov <stiggpwnz@gmail.com>
2019-03-17 15:20:15 +0300

    night mode

--- app/src/main/java/lol/adel/graph/App.kt
+++ app/src/main/java/lol/adel/graph/App.kt
@@ -1,10 +1,11 @@
 package lol.adel.graph
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
